15b4bb3f049400190b3337bd926ba7761dd1407c
Niranjankhatavkar <niranjankhatavkar07@gmail.com>
2026-05-07 20:51:16 +0530

    Add files via upload

---
+++ PointerDemo2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+
+    char ch ='A';
+    char *cptr = &ch;
+
+    int i = 11;
+    int *iptr = &i;
+    
+    float f = 90.99f;
+    float *fptr = &f;
+    
+    double d = 78.67890;
+    double *dptr = &d;
+
+    printf("%c\n",*cptr);
+    printf("%d\n",*iptr);
+    printf("%f\n",*fptr);
+    printf("%lf",*dptr);
+
+
+
+
+
+
+    return 0;
+
+}
